6b7008bdbe551654ed0e91bacae1ad43c070e1a9
dhilipan414 <dhilipan17082000@hotmail.com>
2026-01-31 20:27:34 +0530

    requirements.txt directory change

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from sqlalchemy.testing import exclude
+
 
 from .serializers import SignupSerializer
 from .serializers import CheckPassword
